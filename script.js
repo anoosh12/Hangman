30ce9463c179e050f9965e52ed9d9e98ab11fc62
Nathan Baker <nathanmbaker@gmail.com>
2018-01-03 10:47:07 -0800

    final commit

--- script.js
+++ script.js
@@ -1,35 +1,72 @@
-
-var letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 var word = " ";
-var words = ["bro","mountain","cameroon","nigeria","football","california"];
-var guesses = 10;
-var guessedLetters = [];
+var words = ["mountain","cameroon","california"];
+var guesses = 8;
+var guessedLetters = [ ];
 
 
 function startGame(){
-    word = words[Math.floor(Math.random() * words.length)];
+    guessedLetters = [ ];
+    word = document.getElementById("pickDifficulty").value;
     printWord();
-}
-
-function printWord(){
-    var retWord = " ";
-    for(var i = 0; i < word.length; i++) {
-        if (guessedLetters.indexOf(word[i]) === -1) {
-            retWord += "_ ";
-        } else {
-            retWord += word[i]
-        }
-
-    }
-    console.log(retWord);
-    document.getElementById("board").innerHTML = retWord;
+    console.log(word);
+    console.log(guessedLetters);
 }
 
 function guessLetter(){
-var letter = document.getElementById("guessBox").value;
-    guessedLetters.push(letter);
-    printWord();
+    var guessed = "";
+    var win = "" ;
+    var letter = document.getElementById("guessBox").value;
+    var guessRight = checkGuesses(letter);
+    if(guessRight === true) {
+        guessedLetters.push(letter);
+        var underscoreWord = printWord();
+        if(underscoreWord.indexOf("_") === -1){
+            win += "You Win!";
+        }
+        document.getElementById("winner").innerHTML = win;
+        lowerGuesses(letter);
+        document.getElementById("guessedLetters").innerHTML = guessed + "Letters Guessed: " + guessedLetters;
+        document.getElementById("alreadyGuessed").innerHTML = "";
+    }
+
 }
 
+function printWord() {
+    var retWord = " ";
+    for (var i = 0; i < word.length; i++) {
+        if (guessedLetters.indexOf(word[i]) === -1) {
+            retWord += "_ ";
+        } else {
+            retWord += word[i];
+        }
+    }
+    console.log(retWord);
+    document.getElementById("guessed").innerHTML = retWord;
+    return retWord;
+}
 
+function checkGuesses(letter){
+    var incorrect = " ";
+    if(guessedLetters.indexOf(letter) !== -1) {
+        incorrect += " You already guessed that letter!! ";
+        document.getElementById("alreadyGuessed").innerHTML = incorrect;
+        return false;
+    }else{
+        return true;
+    }
+
+}
+
+function lowerGuesses(letter){
+    var lose = " ";
+    var retGuesses = " ";
+    if(word.indexOf(letter) === -1){
+        guesses--;
+    }
+    document.getElementById("guesses").innerHTML = retGuesses + "Guesses Remaining:" + guesses;
+    if(guesses === 0){
+        lose += "Unfortunately, You have Lost.";
+    }
+    document.getElementById("loser").innerHTML = lose;
+}
 
